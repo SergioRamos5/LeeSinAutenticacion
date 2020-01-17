ffc3050cefb9b972f6743b8865757fc8c4f49b1f
Sergio Ramos Santonja <sergioramos55@outlook.es>
2020-01-17 10:45:40 +0100

    Continuar

--- app/src/main/java/com/example/leesinautenticacion/MainActivity.java
+++ app/src/main/java/com/example/leesinautenticacion/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.leesinautenticacion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etTexto;
+    Button btEnviar;
+    TextView tvSalida;
+    ChildEventListener childEventListener;
+    FirebaseDatabase firebaseDatabase;
+    Adapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etTexto = findViewById(R.id.etTexto);
+        tvSalida = findViewById(R.id.tvSalida);
+        btEnviar = findViewById(R.id.btEnviar);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference dato = FirebaseDatabase.getInstance().getReference().child("items").child("msg");
     }
 }
