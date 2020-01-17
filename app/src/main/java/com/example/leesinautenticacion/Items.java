ed3b5e6dc213e27a9883c0731c29151d242d1161
Sergio Ramos Santonja <sergioramos55@outlook.es>
2020-01-17 13:54:54 +0100

    Continuar

---
+++ app/src/main/java/com/example/leesinautenticacion/Items.java
@@ -0,0 +1,17 @@
+package com.example.leesinautenticacion;
+
+public class Items  {
+    private String msg;
+
+    public Items(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
--- app/src/main/java/com/example/leesinautenticacion/MainActivity.java
+++ app/src/main/java/com/example/leesinautenticacion/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.leesinautenticacion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +39,28 @@ public class MainActivity extends AppCompatActivity {
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference dato = FirebaseDatabase.getInstance().getReference().child("items").child("msg");
+
+        btEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!etTexto.getText().toString().isEmpty())
+                {
+                    firebaseDatabase.getReference("items").push().setValue(new Items(etTexto.getText().toString()));
+                }
+            }
+        });
+
+        dato.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Toast.makeText(getApplicationContext(),(String)dataSnapshot.getValue(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
